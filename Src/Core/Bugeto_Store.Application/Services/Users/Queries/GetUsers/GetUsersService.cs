f2efaf78d13c5d44d066b9c3b4a6b61b76990770
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-04-23 15:36:29 +0430

    Application layer

---
+++ Src/Core/Bugeto_Store.Application/Services/Users/Queries/GetUsers/GetUsersService.cs
@@ -0,0 +1,37 @@
+﻿using Bugeto_Store.Application.Interfaces.Contexts;
+using Bugeto_Store.Common;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bugeto_Store.Application.Services.Users.Queries.GetUsers
+{
+    public class GetUsersService : IGetUsersService
+    {
+        private readonly IDataBaseContext _context;
+        public GetUsersService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+        public ResultGetUserDto Execute(RequestGetUserDto request)
+        {
+            var users = _context.Users.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(request.SearchKey))
+            {
+                users = users.Where(q => q.FullName.Contains(request.SearchKey) && q.Email.Contains(request.SearchKey));
+            }
+            int rowsCount = 0;
+            var usersList = users.ToPaged(request.Page, 20, out rowsCount).Select(q => new GetUsersDto
+            {
+                Email = q.Email,
+                FullName = q.FullName,
+                Id = q.Id,
+            }).ToList();
+
+            return new ResultGetUserDto
+            {
+                Rows = rowsCount,
+                Users = usersList
+            };
+        }
+    }
+}
