6af228f88db9a8e479df43a30b96cae293c7179d
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-04-23 16:09:08 +0430

    create ResultDTO class in common project

---
+++ Src/Common/Bugeto_Store.Common/Dto/ResultDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugeto_Store.Common.Dto
+{
+    public class ResultDTO
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+
+}
